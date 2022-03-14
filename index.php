a5750a42b393505219d953871cc9417fac993c33
Fabionic <98551868+Fabionic@users.noreply.github.com>
2022-03-14 23:07:31 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$movieName = 'Indiana Jones and the Last Crusade';
+echo $movieName; 
+
+$movieViewing = true;
+echo $movieViewing;
+
+$yearOfMovie = 1989;
+echo $yearOfMovie;
+
+$noteSiteImdb = 8.3/10;
+echo $noteSiteImdb;
